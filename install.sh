d9239f20df6ec322727a9e70b00f1aa55e92169f
turneps403 <turneps403@mail.ru>
2020-10-20 09:23:52 +0300

    - remove unnessesary things

--- install.sh
+++
@@ -1,7 +0,0 @@
-helm repo add traefik https://helm.traefik.io/traefik
-helm repo update
-helm install \
-    --set="additionalArguments={--log.level=DEBUG}" \
-    traefik traefik/traefik \
-    -f values.yaml
-kubectl apply -f dashboard.yaml
